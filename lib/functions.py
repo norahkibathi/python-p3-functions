846b42f8e981f469f0c8f4fb6808ca5a569cea0c
norahkibathi <norah.njeri@student.moringaschool.com>
2024-03-30 13:11:40 +0300

    completed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Norah")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default("NJesh")
 
 def add(num1, num2):
-    pass
+    sum = num1 + num2
+    print(sum)
+    return sum
+add(40, 500)    
 
 def halve(number):
-    pass
+    my_half = number/2
+    print(my_half)
+    return my_half
+halve(8)    
